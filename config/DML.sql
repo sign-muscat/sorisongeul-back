7b702392f45acfd63edfa5487b157a1e29a4b409
ddalla0425 <hiwgod92@gmail.com>
2024-07-31 19:28:02 +0900

    setting: modify table / create dummy DML and modify DDL

---
+++ config/DML.sql
@@ -0,0 +1,14 @@
+
+-- GameVoice : 너의 목소리가 보여 개발용 dummy DML
+INSERT INTO sorisonsoon.game_voice (voice_id, answer, question, category, difficulty) VALUES (1, '안녕히 계세요 여러분', '/question_voice/cartoons/question_1.mp4', 'CARTOONS', 'LEVEL_1');
+INSERT INTO sorisonsoon.game_voice (voice_id, answer, question, category, difficulty) VALUES (2, '왜 이러는 걸까요', '/question_voice/comedies/question_1.mp4', 'COMEDIES', 'LEVEL_1');
+INSERT INTO sorisonsoon.game_voice (voice_id, answer, question, category, difficulty) VALUES (3, '쓰레기들아 아아아악', '/question_voice/dramas/question_1.mp4', 'DRAMAS', 'LEVEL_1');
+INSERT INTO sorisonsoon.game_voice (voice_id, answer, question, category, difficulty) VALUES (4, '아 안돼', '/question_voice/memes/question_1.mp4', 'MEMES', 'LEVEL_1');
+INSERT INTO sorisonsoon.game_voice (voice_id, answer, question, category, difficulty) VALUES (5, '오 그러셨어요', '/question_voice/memes/question_2.mp4', 'MEMES', 'LEVEL_2');
+INSERT INTO sorisonsoon.game_voice (voice_id, answer, question, category, difficulty) VALUES (6, '오케이 거기까지', '/question_voice/memes/question_3.mp4', 'MEMES', 'LEVEL_3');
+INSERT INTO sorisonsoon.game_voice (voice_id, answer, question, category, difficulty) VALUES (7, '오 씨 아메리칸스타일', '/question_voice/movies/question_1.mp4', 'MOVIES', 'LEVEL_1');
+INSERT INTO sorisonsoon.game_voice (voice_id, answer, question, category, difficulty) VALUES (8, '에이 이런 좆같은', '/question_voice/movies/question_2.mp4', 'MOVIES', 'LEVEL_1');
+INSERT INTO sorisonsoon.game_voice (voice_id, answer, question, category, difficulty) VALUES (9, '어이가 없네', '/question_voice/movies/question_3.mp4', 'MOVIES', 'LEVEL_2');
+INSERT INTO sorisonsoon.game_voice (voice_id, answer, question, category, difficulty) VALUES (10, '워뗘 후달려', '/question_voice/movies/question_4.mp4', 'MOVIES', 'LEVEL_2');
+INSERT INTO sorisonsoon.game_voice (voice_id, answer, question, category, difficulty) VALUES (11, '야 이 개새끼야', '/question_voice/movies/question_5.mp4', 'MOVIES', 'LEVEL_3');
+INSERT INTO sorisonsoon.game_voice (voice_id, answer, question, category, difficulty) VALUES (12, '이 아저씨가 어따 약을 팔아', '/question_voice/movies/question_6.mp4', 'MOVIES', 'LEVEL_3');
--- src/main/java/com/sorisonsoon/gamevoice/domain/type/GameVoiceCategory.java
+++ src/main/java/com/sorisonsoon/gamevoice/domain/type/GameVoiceCategory.java
@@ -3,7 +3,7 @@ package com.sorisonsoon.gamevoice.domain.type;
 public enum GameVoiceCategory {
     DRAMAS,     // 드라마
     MOVIES,     // 영화
-    COMEDIES,   // 예능
+    COMEDIES,   // 예능, 개그
     CARTOONS,   // 만화
     BOOKS,      // 책
     MEMES       // 밈

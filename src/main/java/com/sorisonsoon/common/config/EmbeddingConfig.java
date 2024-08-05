ba1b1f7dc83bde9369ac5b190d43050106910db7
ddalla0425 <hiwgod92@gmail.com>
2024-08-05 17:08:39 +0900

    feature: sentence similarity model connect in spring ai and model test

--- src/main/java/com/sorisonsoon/common/config/EmbeddingConfig.java
+++ src/main/java/com/sorisonsoon/common/config/EmbeddingConfig.java
@@ -1,25 +1,24 @@
 package com.sorisonsoon.common.config;
 
 
-import com.sorisonsoon.common.TransformersEmbeddingModel;
+import org.springframework.ai.transformers.TransformersEmbeddingModel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class EmbeddingConfig {
 
-    //예제 테스트용 빈
-//    @Bean
-//    public TransformersEmbeddingModel transformersEmbeddingModel() {
-//        return new TransformersEmbeddingModel();
-//    }
+    @Bean
+    public TransformersEmbeddingModel transformersEmbeddingModel() {
+        return new TransformersEmbeddingModel();
+    }
 
     @Bean(name = "customEmbeddingModel")
     public TransformersEmbeddingModel embeddingModel() throws Exception {
         TransformersEmbeddingModel embeddingModel = new TransformersEmbeddingModel();
         embeddingModel.setModelResource("classpath:models/koSentenceTransformers/onnx-output-folder/model.onnx");
         embeddingModel.setTokenizerResource("classpath:models/koSentenceTransformers/onnx-output-folder/tokenizer.json");
-        embeddingModel.setModelOutputName("sentence_embedding");
+        embeddingModel.setModelOutputName("token_embeddings");
         embeddingModel.afterPropertiesSet();
 
         return embeddingModel;

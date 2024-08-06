df4cc377dbc0807e4869890dedaf599c40f96282
ddalla0425 <hiwgod92@gmail.com>
2024-08-06 11:41:34 +0900

    feature: random question submission feature success

--- src/main/java/com/sorisonsoon/gamevoice/controller/GameVoiceController.java
+++ src/main/java/com/sorisonsoon/gamevoice/controller/GameVoiceController.java
@@ -1,11 +1,29 @@
 package com.sorisonsoon.gamevoice.controller;
 
+import com.sorisonsoon.common.domain.type.GameDifficulty;
+import com.sorisonsoon.common.dto.response.ApiResponse;
+import com.sorisonsoon.gamevoice.dto.response.GameVoiceQuestionResponse;
+import com.sorisonsoon.gamevoice.service.GameVoiceService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/")
+@RequestMapping("/api/v1/voice")
 public class GameVoiceController {
+
+    private final GameVoiceService gameVoiceService;
+
+    //랜덤 문제 조회
+    @GetMapping("/question/{difficulty}")
+    public ResponseEntity<ApiResponse> getGameVideoRandomQuestion(@PathVariable final GameDifficulty difficulty) {
+        final GameVoiceQuestionResponse gameVoiceQuestionResponse = gameVoiceService.findVoiceGameRandomQuestion(difficulty);
+        return ResponseEntity.ok(ApiResponse.success("랜덤으로 문제를 가져왔습니다.",gameVoiceQuestionResponse));
+    }
+
+
 }
--- src/main/java/com/sorisonsoon/gamevoice/service/GameVoiceService.java
+++ src/main/java/com/sorisonsoon/gamevoice/service/GameVoiceService.java
@@ -1,5 +1,8 @@
 package com.sorisonsoon.gamevoice.service;
 
+import com.sorisonsoon.common.domain.type.GameDifficulty;
+import com.sorisonsoon.gamevoice.domain.repository.GameVoiceRepository;
+import com.sorisonsoon.gamevoice.dto.response.GameVoiceQuestionResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -9,4 +12,8 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class GameVoiceService {
 
+    private final GameVoiceRepository gameVoiceRepository;
+    public GameVoiceQuestionResponse findVoiceGameRandomQuestion(GameDifficulty difficulty) {
+        return gameVoiceRepository.getRandomVoiceByDifficulty(difficulty);
+    }
 }

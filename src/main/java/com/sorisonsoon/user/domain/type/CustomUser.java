1afd6064f02881b5f41cce62b1aef1ce34363218
ddalla0425 <hiwgod92@gmail.com>
2024-08-14 17:15:04 +0900

    setting: user custom setting

--- src/main/java/com/sorisonsoon/user/domain/type/CustomUser.java
+++ src/main/java/com/sorisonsoon/user/domain/type/CustomUser.java
@@ -3,7 +3,6 @@ package com.sorisonsoon.user.domain.type;
 import lombok.Getter;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collections;
 
@@ -12,13 +11,9 @@ public class CustomUser extends User {
 
     private Long userId;
 
-    public CustomUser(Long userId, String username, String password, UserRole role) {
-        super(username, password, Collections.singletonList(new SimpleGrantedAuthority(role.name())));
+    public CustomUser(Long userId, String id, String password, UserRole role) {
+        super(id, password, Collections.singletonList(new SimpleGrantedAuthority(role.name())));
         this.userId = userId;
     }
 
-    public CustomUser(Long userId, UserDetails userDetails) {
-        super(userDetails.getUsername(), userDetails.getPassword(), userDetails.getAuthorities());
-        this.userId = userId;
-    }
 }

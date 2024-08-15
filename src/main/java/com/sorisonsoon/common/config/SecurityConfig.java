8fa11443de82413cafaf422589ef4dd9793c03f8
ddalla0425 <hiwgod92@gmail.com>
2024-08-16 04:09:34 +0900

    setting: security setting

--- src/main/java/com/sorisonsoon/common/config/SecurityConfig.java
+++ src/main/java/com/sorisonsoon/common/config/SecurityConfig.java
@@ -1,15 +1,31 @@
 package com.sorisonsoon.common.config;
 
+import com.sorisonsoon.common.security.filter.CustomAuthenticationFilter;
+import com.sorisonsoon.common.security.filter.JwtAuthenticationFilter;
+import com.sorisonsoon.common.security.hadler.JwtAccessDeniedHandler;
+import com.sorisonsoon.common.security.hadler.JwtAuthenticationEntryPoint;
+import com.sorisonsoon.common.security.util.TokenUtils;
 import com.sorisonsoon.user.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableMethodSecurity
@@ -17,6 +33,8 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     private final AuthService authService;
+    private final PasswordEncoder passwordEncoder;
+    private final TokenUtils tokenUtils;
 
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -29,39 +47,97 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> {
                     auth.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll();
                     auth.requestMatchers(HttpMethod.GET,
-                                        "/images/**", "/api/v1/find/**",
-                                        "/api/v1/notice/list", "/api/v1/community/list",
-                                        "/api/v1/sentence/game_start", "/api/v1/sign/game_start",
-                                        "/api/v1/voice/question"
-                                    )
-                                            .permitAll();
+                                        "/api/swagger-ui/**", "/api/v1/api-docs/**",
+                                        "/images/**", "/api/v1/community/list",
+                                        "/api/v1/sentence/game_start", "/api/v1/sign/game_start", "/api/v1/voice/question"
+                                    ).permitAll();
                     auth.requestMatchers(HttpMethod.POST,
-                                         "/api/v1/members/new/**", "/api/v1/members/check/**",
                                          "/api/v1/login", "/api/v1/token/issue",
-                                         "/api/v1/verify/**")
-                                            .permitAll();
+                                         "/api/v1/users/new", "/api/v1/users/mailConfirm", "/api/v1/users/verifyCode",
+                                         "/api/v1/verify/**").permitAll();
 
                     auth.requestMatchers(HttpMethod.GET,
-                                        "api/v1/friends/**", "/api/members/**",
-                                        "api/v1/sentence/**", "api/v1/sign/**",
-                                        "api/v1/voice/**","api/v1/page/**"
+                                        "/api/v1/search/**",
+                                        "/api/v1/friends/**",
+                                        "/api/v1/sentence/**", "/api/v1/sign/**",
+                                        "/api/v1/voice/**","/api/v1/page/**"
                                     ).hasAnyAuthority("FREE_USER", "PREMIUM_USER", "ADMIN");
 
                     auth.requestMatchers(HttpMethod.POST,
                                         "/api/v1/logout",
-                                        "api/v1/friends/**", "/api/members/**",
-                                        "api/v1/sentence/**", "api/v1/sign/**", "" )
+                                        "api/v1/friends/**", "/api/v1/page/**",
+                                        "api/v1/sentence/**", "api/v1/sign/**","/api/v1/voice/**" )
                                             .hasAnyAuthority("FREE_USER", "PREMIUM_USER", "ADMIN");
 
                     auth.requestMatchers(HttpMethod.POST,
-                                        "api/v1/premium")
+                                        "api/v1/premium/**")
                                             .hasAuthority("FREE_USER");
 
                     auth.requestMatchers("/api/v1/notice/**").hasAuthority("ADMIN");
                     auth.anyRequest().authenticated();
                 })
+                .addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(customAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling(exceptionHandling -> {
+                    exceptionHandling.accessDeniedHandler(jwtAccessDeniedHandler());
+                    exceptionHandling.authenticationEntryPoint(jwtAuthenticationEntryPoint());
+                })
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .build();
     }
 
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        /* TODO :: 추후 설정 */
+//        corsConfiguration.setAllowedOrigins(List.of("http://localhost:3000/"));
+        corsConfiguration.setAllowedOrigins(List.of("*"));
+        corsConfiguration.setAllowedMethods(Arrays.asList("GET", "PUT", "POST", "DELETE"));
+        corsConfiguration.setAllowedHeaders(Arrays.asList("Access-Control-Allow-Origin",
+                "Access-Control-Allow-Headers",
+                "Content-Type",
+                "Authorization",
+                "X-Requested-With",
+                "Access-Token",
+                "Refresh-Token"));
+        corsConfiguration.setExposedHeaders(Arrays.asList("Access-Token", "Refresh-Token"));
+        corsConfiguration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return source;
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder);
+        provider.setUserDetailsService(authService);
+        return new ProviderManager(provider);
+    }
+
+    @Bean
+    CustomAuthenticationFilter customAuthenticationFilter() {
+
+        CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter();
+        customAuthenticationFilter.setAuthenticationManager(authenticationManager());
+
+        return customAuthenticationFilter;
+    }
+
+    @Bean
+    JwtAuthenticationFilter jwtAuthenticationFilter() {
+        return new JwtAuthenticationFilter(authService, tokenUtils);
+    }
+
+    @Bean
+    JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint() {
+        return new JwtAuthenticationEntryPoint();
+    }
+
+    @Bean
+    JwtAccessDeniedHandler jwtAccessDeniedHandler() {
+        return new JwtAccessDeniedHandler();
+    }
 
 }
--- src/main/java/com/sorisonsoon/common/security/util/TokenUtils.java
+++ src/main/java/com/sorisonsoon/common/security/util/TokenUtils.java
@@ -1,3 +1,113 @@
+//package com.sorisonsoon.common.security.util;
+//
+//import io.jsonwebtoken.Claims;
+//import io.jsonwebtoken.Jwts;
+//import io.jsonwebtoken.SignatureAlgorithm;
+//import io.jsonwebtoken.io.Decoders;
+//import io.jsonwebtoken.security.Keys;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.stereotype.Component;
+//
+//import java.security.Key;
+//import java.util.Date;
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//@Slf4j
+//@Component
+//public class TokenUtils {
+//
+//    private static String secret;
+//
+//    private static Long accessTokenExpiration;
+//
+//    private static Long refreshTokenExpiration;
+//
+//
+//    private static final String BEARER = "Bearer ";
+//
+//
+//    @Value("${jwt.secret}")
+//    public void setJwtSecretKey(String secret) {
+//        TokenUtils.secret = secret;
+//    }
+//
+//    @Value("${jwt.access.expiration}")
+//    public void setAccessTokenExpiration(Long accessTokenExpiration) {
+//        TokenUtils.accessTokenExpiration = accessTokenExpiration;
+//    }
+//
+//    @Value("${jwt.refresh.expiration}")
+//    public void setRefreshTokenExpiration(Long refreshTokenExpiration) {
+//        TokenUtils.refreshTokenExpiration = refreshTokenExpiration;
+//    }
+//
+//    public static boolean isValidToken(String token) {
+//        try {
+//            Jwts.parserBuilder().setSigningKey(createSignature()).build().parseClaimsJws(token);
+//            return true;
+//        } catch (Exception e) {
+//            log.error("유효하지 않은 토큰입니다. {}", e.getMessage());
+//            return false;
+//        }
+//    }
+//
+////    public static String getToken(String token) {
+////        return (token != null && token.startsWith(BEARER))
+////                ? token.replace(BEARER, "") : null;
+////    }
+//    public static String getToken(String token) {
+//        if (token != null && token.startsWith(BEARER)) {
+//            return token.substring(BEARER.length());
+//        }
+//        return null;
+//    }
+//
+//    public static String getIdFromToken(String accessToken) {
+//        return Jwts.parserBuilder()
+//                   .setSigningKey(createSignature())
+//                   .build()
+//                   .parseClaimsJws(accessToken)
+//                   .getBody()
+//                   .get("id").toString();
+//    }
+//
+//    public static String createAccessToken(Map<String, Object> userInfo) {
+//
+//        Claims claims = Jwts.claims().setSubject("AccessToken");
+//        claims.putAll(userInfo);
+//
+//        return Jwts.builder()
+//                   .setHeader(createHeader())
+//                   .setClaims(claims)
+//                   .setExpiration(new Date(System.currentTimeMillis() + accessTokenExpiration))
+//                   .signWith(createSignature(), SignatureAlgorithm.HS512)
+//                   .compact();
+//    }
+//
+//    public static String createRefreshToken() {
+//        return Jwts.builder()
+//                   .setHeader(createHeader())
+//                   .setSubject("RefreshToken")
+//                   .setExpiration(new Date(System.currentTimeMillis() + refreshTokenExpiration))
+//                   .signWith(createSignature(), SignatureAlgorithm.HS512)
+//                   .compact();
+//    }
+//
+//    private static Key createSignature() {
+//        byte[] keyBytes = Decoders.BASE64.decode(secret);
+//        return Keys.hmacShaKeyFor(keyBytes);
+//    }
+//
+//    private static Map<String, Object> createHeader() {
+//        Map<String, Object> header = new HashMap<>();
+//        header.put("type", "jwt");
+//        header.put("date", System.currentTimeMillis());
+//        return header;
+//    }
+//}
+
 package com.sorisonsoon.common.security.util;
 
 import io.jsonwebtoken.Claims;
@@ -5,99 +115,91 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.function.Function;
 
-@Slf4j
 @Component
 public class TokenUtils {
 
-    private static String secret;
+    @Value("${jwt.secret}")
+    private String secret;
 
-    private static Long accessTokenExpiration;
+    @Value("${jwt.access.expiration}")
+    private Long accessTokenExpiration;
 
-    private static Long refreshTokenExpiration;
-
+    @Value("${jwt.refresh.expiration}")
+    private Long refreshTokenExpiration;
 
     private static final String BEARER = "Bearer ";
 
-
-    @Value("${jwt.secret}")
-    public void setJwtSecretKey(String secret) {
-        TokenUtils.secret = secret;
-    }
-
-    @Value("${jwt.access.expiration}")
-    public void setAccessTokenExpiration(Long accessTokenExpiration) {
-        TokenUtils.accessTokenExpiration = accessTokenExpiration;
-    }
-
-    @Value("${jwt.refresh.expiration}")
-    public void setRefreshTokenExpiration(Long refreshTokenExpiration) {
-        TokenUtils.refreshTokenExpiration = refreshTokenExpiration;
-    }
-
-    public static boolean isValidToken(String token) {
-        try {
-            Jwts.parserBuilder().setSigningKey(createSignature()).build().parseClaimsJws(token);
-            return true;
-        } catch (Exception e) {
-            log.error("유효하지 않은 토큰입니다. {}", e.getMessage());
-            return false;
-        }
-    }
-
-    public static String getToken(String token) {
-        return (token != null && token.startsWith(BEARER))
-                ? token.replace(BEARER, "") : null;
-    }
-
-    public static String getIdFromToken(String accessToken) {
-        return Jwts.parserBuilder()
-                   .setSigningKey(createSignature())
-                   .build()
-                   .parseClaimsJws(accessToken)
-                   .getBody()
-                   .get("id").toString();
-    }
-
-    public static String createAccessToken(Map<String, Object> userInfo) {
-
-        Claims claims = Jwts.claims().setSubject("AccessToken");
-        claims.putAll(userInfo);
-
-        return Jwts.builder()
-                   .setHeader(createHeader())
-                   .setClaims(claims)
-                   .setExpiration(new Date(System.currentTimeMillis() + accessTokenExpiration))
-                   .signWith(createSignature(), SignatureAlgorithm.HS512)
-                   .compact();
-    }
-
-    public static String createRefreshToken() {
-        return Jwts.builder()
-                   .setHeader(createHeader())
-                   .setSubject("RefreshToken")
-                   .setExpiration(new Date(System.currentTimeMillis() + refreshTokenExpiration))
-                   .signWith(createSignature(), SignatureAlgorithm.HS512)
-                   .compact();
-    }
-
-    private static Key createSignature() {
+    private Key createSignature() {
         byte[] keyBytes = Decoders.BASE64.decode(secret);
         return Keys.hmacShaKeyFor(keyBytes);
     }
 
-    private static Map<String, Object> createHeader() {
-        Map<String, Object> header = new HashMap<>();
-        header.put("type", "jwt");
-        header.put("date", System.currentTimeMillis());
-        return header;
+    // Access Token 생성
+    public String createAccessToken(UserDetails userDetails) {
+        return doCreateToken(userDetails.getUsername(), accessTokenExpiration);
     }
+
+    // Refresh Token 생성
+    public String createRefreshToken() {
+        return doCreateToken(null, refreshTokenExpiration);
+    }
+
+    // 실제 토큰 값 반환
+    public static String getToken(String token) {
+        return (token != null && token.startsWith(BEARER))
+                ? token.substring(BEARER.length()) : null;
+    }
+
+    // JWT 토큰에서 사용자 이름 추출
+    public String getUsernameFromToken(String token) {
+        return getClaimFromToken(token, Claims::getSubject);
+    }
+
+    // JWT 토큰 유효성 검증
+    public Boolean isValidToken(String token, UserDetails userDetails) {
+        final String username = getUsernameFromToken(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    // 토큰 만료 여부 확인
+    private Boolean isTokenExpired(String token) {
+        final Date expiration = getExpirationDateFromToken(token);
+        return expiration.before(new Date());
+    }
+
+    // 토큰 만료 날짜 추출
+    private Date getExpirationDateFromToken(String token) {
+        return getClaimFromToken(token, Claims::getExpiration);
+    }
+
+    // JWT 토큰에서 클레임 추출
+    public <T> T getClaimFromToken(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = getAllClaimsFromToken(token);
+        return claimsResolver.apply(claims);
+    }
+
+    // 모든 클레임을 추출하는 메서드
+    private Claims getAllClaimsFromToken(String token) {
+        return Jwts.parserBuilder().setSigningKey(createSignature()).build().parseClaimsJws(token).getBody();
+    }
+
+    // 토큰 생성 메서드
+    private String doCreateToken(String subject, Long expirationTime) {
+        return Jwts.builder()
+                .setSubject(subject)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + expirationTime))
+                .signWith(createSignature(), SignatureAlgorithm.HS512)
+                .compact();
+    }
+
 }
+

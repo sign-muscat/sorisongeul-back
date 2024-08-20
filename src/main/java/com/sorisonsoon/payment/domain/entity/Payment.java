bde0cd9a3ca919f5a6d56b2f61dde28ef53708eb
dongjunsong <papi64129955@gmail.com>
2024-08-20 22:23:32 +0900

    결제 기능 일단 성공

--- src/main/java/com/sorisonsoon/payment/domain/entity/Payment.java
+++ src/main/java/com/sorisonsoon/payment/domain/entity/Payment.java
@@ -1,25 +1,49 @@
+// package com.sorisonsoon.payment.domain.entity;
+
+// import jakarta.persistence.*;
+// import lombok.AccessLevel;
+// import lombok.NoArgsConstructor;
+// import org.springframework.data.annotation.CreatedDate;
+
+// import java.time.LocalDateTime;
+
+// @Entity
+// @Table(name = "payment")
+// @NoArgsConstructor(access = AccessLevel.PROTECTED)
+// public class Payment {
+
+//     @Id
+//     @GeneratedValue(strategy = GenerationType.IDENTITY)
+//     @Column(name = "payment_id")
+//     private Long paymentId;
+
+//     private int payerId;
+//     private int amount;
+
+//     @CreatedDate
+//     private LocalDateTime payedAt;
+// }
 package com.sorisonsoon.payment.domain.entity;
 
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.CreatedDate;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "payment")
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Setter
 public class Payment {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "payment_id")
     private Long paymentId;
 
-    private int payerId;
+    private Long payerId;
     private int amount;
-
-    @CreatedDate
     private LocalDateTime payedAt;
+
+    // 필요한 경우 추가 필드들...
 }

875fffeba2ed3fbab4fb0861a59ed86065fc5f7f
dongjunsong <papi64129955@gmail.com>
2024-08-20 22:23:32 +0900

    결제 기능 일단 성공

--- src/main/java/com/sorisonsoon/payment/domain/repository/PaymentRepository.java
+++ src/main/java/com/sorisonsoon/payment/domain/repository/PaymentRepository.java
@@ -1,8 +1,18 @@
+// package com.sorisonsoon.payment.domain.repository;
+
+// import com.sorisonsoon.payment.domain.entity.Payment;
+// import org.springframework.data.jpa.repository.JpaRepository;
+
+// public interface PaymentRepository extends JpaRepository<Payment, Long> {
+
+// }
 package com.sorisonsoon.payment.domain.repository;
 
 import com.sorisonsoon.payment.domain.entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PaymentRepository extends JpaRepository<Payment, Long> {
+import java.util.List;
 
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+    List<Payment> findByPayerId(long payerId);
 }

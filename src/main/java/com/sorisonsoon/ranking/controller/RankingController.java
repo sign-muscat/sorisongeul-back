b29305267e481018ab999d457902a35acf913a6f
dongjunsong <papi64129955@gmail.com>
2024-08-21 20:00:45 +0900

    랭킹 정리 부분

--- src/main/java/com/sorisonsoon/ranking/controller/RankingController.java
+++ src/main/java/com/sorisonsoon/ranking/controller/RankingController.java
@@ -1,11 +1,31 @@
 package com.sorisonsoon.ranking.controller;
 
+import com.sorisonsoon.common.domain.type.GameCategory;
+import com.sorisonsoon.ranking.dto.RankingDTO;
+import com.sorisonsoon.ranking.service.RankingService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/")
+@RequestMapping("/rankings")
 public class RankingController {
+    private final RankingService rankingService;
+
+    @PostMapping
+    public ResponseEntity<Void> saveScore(@RequestParam Long userId,
+                                          @RequestParam GameCategory category,
+                                          @RequestParam double score) {
+        rankingService.save(userId, category, score);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/{category}")
+    public ResponseEntity<List<RankingDTO>> getTop10(@PathVariable GameCategory category) {
+        List<RankingDTO> rankings = rankingService.getTop10ByCategory(category);
+        return ResponseEntity.ok(rankings);
+    }
 }
--- src/main/java/com/sorisonsoon/ranking/service/RankingService.java
+++ src/main/java/com/sorisonsoon/ranking/service/RankingService.java
@@ -3,9 +3,13 @@ package com.sorisonsoon.ranking.service;
 import com.sorisonsoon.common.domain.type.GameCategory;
 import com.sorisonsoon.ranking.domain.entity.Ranking;
 import com.sorisonsoon.ranking.domain.repository.RankingRepository;
+import com.sorisonsoon.ranking.dto.RankingDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
@@ -20,5 +24,13 @@ public class RankingService {
                 (int) Math.round(score * 100)
         );
         rankingRepository.save(newRanking);
+    }
+
+    @Transactional(readOnly = true)
+    public List<RankingDTO> getTop10ByCategory(GameCategory category) {
+        List<Ranking> rankings = rankingRepository.findTop10ByCategory(category);
+        return rankings.stream()
+                .map(ranking -> new RankingDTO(ranking.getUserId(), ranking.getScore()))
+                .collect(Collectors.toList());
     }
 }

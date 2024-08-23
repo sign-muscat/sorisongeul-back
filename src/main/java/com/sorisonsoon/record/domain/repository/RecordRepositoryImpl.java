1b8153a975e0638b6428f7df59e5dc3c93828dd8
KimTaekgyoon <142713364+KimTaekgyoon@users.noreply.github.com>
2024-08-23 18:12:51 +0900

    refact: 맞수수 가이드 이미지 오류 해결 및 모델 평가 방법 변경 및 도소탐 제출 기능 수정(#60) (#61)
    Co-authored-by: ppyye <154950167+ppyye@users.noreply.github.com>

--- src/main/java/com/sorisonsoon/record/domain/repository/RecordRepositoryImpl.java
+++ src/main/java/com/sorisonsoon/record/domain/repository/RecordRepositoryImpl.java
@@ -2,10 +2,24 @@ package com.sorisonsoon.record.domain.repository;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import com.sorisonsoon.record.domain.entity.QRecord;
+
+import java.time.LocalDateTime;
 
 @RequiredArgsConstructor
 public class RecordRepositoryImpl implements RecordRepositoryCustom {
+
     private final JPAQueryFactory queryFactory;
 
+    @Override
+    public void deleteAllByPlayerIdAndCreatedAtBetween(Long playerId, LocalDateTime start, LocalDateTime end) {
+        QRecord record = QRecord.record;
 
+        // 해당 플레이어의 주어진 시간 범위 내의 모든 레코드를 삭제합니다.
+        queryFactory
+            .delete(record)
+            .where(record.playerId.eq(playerId)
+                .and(record.createdAt.between(start, end)))
+            .execute();
+    }
 }
